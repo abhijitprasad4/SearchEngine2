ed529e0de47f07fcb6f96669cba1f613b4ec89b4
abhijitprasad4 <94544539+abhijitprasad4@users.noreply.github.com>
2022-09-14 14:59:38 +0530

    Update main.py
    following the snake case naming convention, removing all the global variables 
    making every function take arguments and return except main
    removed all single letter variables to self explanatory variables
    making address part as variable so that the program can also be used for different set of data

--- SearchEngine2/main.py
+++ SearchEngine2/main.py
@@ -1,21 +1,22 @@
-# Storing all word that have to be ignored in a set
-def IgnoreWord():
-    f = open("english.txt",encoding="utf8")
-    read = f.readlines()
+
+def ignore_word(address):
+    """ Storing all word that have to be ignored in a set"""
+    file = open(address, encoding="utf8")
+    read = file.readlines()
     temp_list = []
     for line in read:
         temp_list.append(line.strip())
-    f.close()
+    file.close()
 
     ignored_word_set = set()
-
-    for x in temp_list:
-        if x not in ignored_word_set:
-            ignored_word_set.add(x)
+    for word in temp_list:
+        if word not in ignored_word_set:
+            ignored_word_set.add(word)
     return ignored_word_set
 
-# removepuc will remove all the punctuation from the word and also lower case it
-def RemovePunc(word):
+
+def remove_punc(word):
+    """remove_punc will remove all the punctuation and also lower case it"""
     new_word = ""
     word = word.lower()
     for letter in word:
@@ -23,47 +24,51 @@ def RemovePunc(word):
             new_word += letter
     return new_word
 
-# now we will loop through all article and it will store article name as key and a dict as value 
-# in that inside dict individual word will be stored as key and a list of its all indexes as value
-# like main_dict = {"k1" : {"katrina" : [1,3,4],"salmon":[2]}}
-# in the nested dict there is a "article-title" key which will store all the title word of that particular article
-# like main_dict = {"k1" : {"katrina" : [1,3,4],"salmon":[2],"article-title":["katrina","kaif"]}}
-def PreProcessing():
+
+def pre_processing(address, no_of_articles, ignored_word_set):
+    """ now we will loop through all article and it will store article name as key and a dict as value
+        in that inside dict individual word will be stored as key and a list of its all indexes as value
+        like main_dict = {"k1" : {"katrina" : [1,3,4],"salmon":[2]}}
+        in the nested dict there is a "article-title" key which will store all the title word of that particular article
+        like main_dict = {"k1" : {"katrina" : [1,3,4],"salmon":[2],"article-title":["katrina","kaif"]}}"""
     main_dict = {}
-    for x in range(1,51):
-        current_article = "k" + str(x)
-        f = open("news_k50/"+current_article+".txt",encoding="utf-8")
-        i = 0
+    for current_no in range(1, no_of_articles+1):
+        current_article = "k" + str(current_no)
+        file = open(address + "/" + current_article + ".txt", encoding="utf-8")
+        title_checker = True
         word_index = 1
-        read = f.readlines()
+        read = file.readlines()
         for line in read:
             for word in line.split():
-                word = RemovePunc(word)
-                if word not in ignored_word_set: 
+                word = remove_punc(word)
+                if word not in ignored_word_set:
                     if current_article not in main_dict:
-                        main_dict[current_article] = {word : [word_index]}
+                        main_dict[current_article] = {word: [word_index]}
                     else:
                         if word in main_dict[current_article]:
                             main_dict[current_article][word] += [word_index]
                         else:
                             main_dict[current_article][word] = [word_index]
-                    # i is use to check if the word is in title or not
-                    if i == 0:
+                    if title_checker:
                         if "article-title" not in main_dict[current_article]:
                             main_dict[current_article]["article-title"] = [word]
                         else:
                             main_dict[current_article]["article-title"] += [word]
                 word_index += 1
-            i += 1
-        f.close()
+            title_checker = False
+        file.close()
     return main_dict
-    # ranking rules that this program follows is
-    # more query word matches in the article and after that it follows
-    # second rule the more queryword matched in the title
-    # and third rule which is the sum of all frequecy of query words
-    # fourth rule is word that occur in starting of the document(article)
+
+    """ranking rules that this program follows is
+       more query word matches in the article and after that it follows
+       second rule the more queryword matched in the title
+       and third rule which is the sum of all frequecy of query words
+       fourth rule is word that occur in starting of the document(article)"""
+
+
 def main():
-    main_dict = PreProcessing()
+    ignored_word_set = ignore_word("english.txt")
+    main_dict = pre_processing("news_k50", 50, ignored_word_set)
     while True:
         user_input = input("Enter your word:- ")
         user_input = user_input.split()
@@ -71,7 +76,7 @@ def main():
         # removing all the word that has to be ignored and also punctuations
         for word in user_input:
             if word not in ignored_word_set:
-                inputed_word_list.append(RemovePunc(word))
+                inputed_word_list.append(remove_punc(word))
         temp_dict = {}
         # queryword means the no. of words that matches with inputed words and words in a particular article
         # title means the no. of inputed words thats in the title of an article
@@ -79,8 +84,8 @@ def main():
         # first_occ means the sum of all indexes of all inputed words in a particular article
 
         for current_article in main_dict:
-            temp_dict[current_article] = [0,0,0,float("inf")]
-                                       # [queryword,title,freq,first_occ]
+            temp_dict[current_article] = [0, 0, 0, float("inf")]
+            # [queryword,title,freq,first_occ]
             for word in inputed_word_list:
                 if word in main_dict[current_article]:
                     # queryword
@@ -89,26 +94,28 @@ def main():
                     if word in main_dict[current_article]["article-title"]:
                         temp_dict[current_article][1] += 1
                     # freq
-                    temp_dict[current_article][2] += len(main_dict[current_article][word])
+                    temp_dict[current_article][2] += len(
+                        main_dict[current_article][word])
                     # occurr
                     if temp_dict[current_article][3] == float("inf"):
                         temp_dict[current_article][3] = main_dict[current_article][word][0]
                     else:
                         temp_dict[current_article][3] += main_dict[current_article][word][0]
-            if temp_dict[current_article] == [0,0,0,float("inf")]:
+            if temp_dict[current_article] == [0, 0, 0, float("inf")]:
                 del temp_dict[current_article]
 
-        #sorting queryword,title, freq in decreasing order and first_occ by increasing order
-        temp_list = sorted(temp_dict.keys(),key=lambda x: (temp_dict[x][0],temp_dict[x][1],temp_dict[x][2],-temp_dict[x][3]), reverse=True)
+        # sorting queryword,title, freq in decreasing order and first_occ by increasing order
+        temp_list = sorted(temp_dict.keys(), key=lambda x: (
+            temp_dict[x][0], temp_dict[x][1], temp_dict[x][2], -temp_dict[x][3]), reverse=True)
 
-        #removing the k part and printing only the no. of articles
+        # removing the k part and printing only the no. of articles
         final_list = []
-        for x in temp_list[:5]:
-            final_list.append(x[1:])
-    
+        for article in temp_list[:5]:
+            final_list.append(article[1:])
+
         print("Articles related to your query are:- ")
         print(final_list)
         print()
-        
-ignored_word_set = IgnoreWord()
+
+
 main()
